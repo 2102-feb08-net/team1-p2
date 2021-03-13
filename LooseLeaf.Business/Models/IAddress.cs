dac424038d89512ae0f8ea3dcaaa7b043865e44e
Bryson Ewell <brysonewell@outlook.com>
2021-03-13 13:35:51 -0500

    Added interfaces to buisness logic

---
+++ LooseLeaf.Business/Models/IAddress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LooseLeaf.Business.Models
+{
+    public interface IAddress
+    {
+        string Address1 { get; }
+
+        string Address2 { get; }
+
+        string City { get; }
+
+        string State { get; }
+
+        string Country { get; }
+
+        int ZipCode { get; }
+    }
+}
---
+++ LooseLeaf.Business/Models/IUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LooseLeaf.Business.Models
+{
+    public interface IUser
+    {
+        string UserName { get; }
+
+        string Email { get; }
+
+        IAddress Address { get; }
+    }
+}
