01e376f26228f74e8ff24a4220299221beaf0b03
Bryson Ewell <brysonewell@outlook.com>
2021-03-15 17:08:52 -0400

    Added BookRepository and interface with BookSearchParams

---
+++ LooseLeaf.Business/IBookRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LooseLeaf.Business.Models;
+
+namespace LooseLeaf.Business
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<IBook>> GetAllBooks(IBookSearchParams searchParams);
+
+        Task AddBook();
+
+        Task<IBook> GetBook(int bookId);
+
+        Task UpdateBook(int bookId);
+    }
+}
