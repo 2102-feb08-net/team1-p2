0db889cd23a970d06790df00feccf7ef4fb40e94
John Werner <jwerner547@gmail.com>
2021-03-15 15:23:54 -0600

    starting integration tests

---
+++ LooseLeaf.Tests/IntegrationTests/LoanRespositoryTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using LooseLeaf.Business.Models;
+using Moq;
+
+namespace LooseLeaf.Tests.IntegrationTests
+{
+    public class LoanRespositoryTests
+    {
+        // get all loans
+        [Fact]
+        public void Loans_GetAll()
+        {
+            // arrange
+
+            // act
+
+            // assert
+        }
+
+        // get loan by id
+
+        // submit a loan request
+
+        // update the satus of a loan
+
+        // get loans by userId
+    }
+}
---
+++ LooseLeaf.Tests/IntegrationTests/TestLooseLeafContextFactory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LooseLeaf.DataAccess;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace LooseLeaf.Tests.IntegrationTests
+{
+    public class TestLooseLeafContextFactory : IDisposable
+    {
+        private DbConnection _conn;
+        private bool _disposedValue;
+
+        private DbContextOptions<LooseLeafContext> CreateOptions()
+        {
+            return new DbContextOptionsBuilder<LooseLeafContext>().UseSqlite(_conn).Options;
+        }
+
+        public LooseLeafContext CreateContext()
+        {
+            if (_conn == null)
+            {
+                _conn = new SqliteConnection("DataSource=:memory:");
+                _conn.Open();
+
+                DbContextOptions<LooseLeafContext> options = CreateOptions();
+                using var context = new LooseLeafContext(options);
+                context.Database.EnsureCreated();
+            }
+
+            return new LooseLeafContext(CreateOptions());
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _conn.Dispose();
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
