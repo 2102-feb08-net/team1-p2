931de02ff78e6fb8f47679cdb7934ff266c07634
Bryson Ewell <brysonewell@outlook.com>
2021-03-14 23:27:00 -0400

    Added Wishlist and tests

---
+++ LooseLeaf.Business/Models/Wishlist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LooseLeaf.Business.Models
+{
+    public class Wishlist : IWishlist
+    {
+        public List<IBook> Books { get; } = new List<IBook>();
+    }
+}
---
+++ LooseLeaf.Tests/WishlistTests.cs
@@ -0,0 +1,38 @@
+﻿using LooseLeaf.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LooseLeaf.Tests
+{
+    public class WishlistTests
+    {
+        [Fact]
+        public void Wishlist_Construct_Pass()
+        {
+            // arrange
+
+            // act
+            IWishlist wishlist = new Wishlist();
+
+            // assert
+            Assert.NotNull(wishlist);
+        }
+
+        [Fact]
+        public void Wishlist_GetBooks()
+        {
+            // arrange
+            IWishlist wishlist = new Wishlist();
+
+            // act
+            var books = wishlist.Books;
+
+            // assert
+            Assert.NotNull(books);
+        }
+    }
+}
