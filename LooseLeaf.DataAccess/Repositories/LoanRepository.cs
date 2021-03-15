eba1bb05cf7f50ee2536e80a79042119cc416906
Bryson Ewell <brysonewell@outlook.com>
2021-03-15 12:52:20 -0400

    Fixed info messages

---
+++ LooseLeaf.DataAccess/Repositories/LoanRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LooseLeaf.DataAccess.Repositories
+{
+    public class LoanRepository
+    {
+        private readonly LooseLeafContext _context;
+
+        public LoanRepository(LooseLeafContext context)
+        {
+            _context = context;
+        }
+    }
+}
