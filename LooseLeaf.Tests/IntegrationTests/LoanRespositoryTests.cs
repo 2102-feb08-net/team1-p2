321369ee1b032ce650a24a507b47bae3ca1cbee7
Bryson Ewell <brysonewell@outlook.com>
2021-03-15 20:50:31 -0400

    Worked on Loan test

--- LooseLeaf.Tests/IntegrationTests/LoanRespositoryTests.cs
+++ LooseLeaf.Tests/IntegrationTests/LoanRespositoryTests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Xunit;
 using LooseLeaf.Business.Models;
 using Moq;
+using LooseLeaf.DataAccess;
 
 namespace LooseLeaf.Tests.IntegrationTests
 {
@@ -20,6 +21,38 @@ namespace LooseLeaf.Tests.IntegrationTests
             // act
 
             // assert
+        }
+
+        [Fact]
+        public async Task Loans_AddLoan()
+        {
+            // arrange
+            using var contextFactory = new TestLooseLeafContextFactory();
+            using LooseLeafContext context = contextFactory.CreateContext();
+
+            IUser lender = new Mock<IUser>().Object;
+            IUser borrower = new Mock<IUser>().Object;
+            IAddress fakeAddress = new Mock<IAddress>().Object;
+            var fakeBook = new Mock<IOwnedBook>();
+            fakeBook.Setup(b => b.Owner).Returns(lender);
+
+            var ownedBooks = new List<IOwnedBook>()
+            {
+                fakeBook.Object
+            };
+
+            var loans = new List<ILoan>()
+            {
+                new Business.Models.Loan(lender,borrower, "Hello", new DateTime(2000,1,2), new DateTime(2000,1,4), fakeAddress, ownedBooks)
+            };
+
+            // act
+            // TODO: Add Loan Repository call
+
+            await context.SaveChangesAsync();
+
+            //assert
+            Assert.Equal(loans.Count, context.Loans.Count());
         }
 
         // get loan by id
