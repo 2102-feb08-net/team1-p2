0434e73b3754d60df707a891d70637c24196bfed
Ashley Brown <1thing.ashleyb@gmail.com>
2021-03-18 20:49:14 -0500

    added GetSpecificBooks

--- LooseLeaf.DataAccess/Repositories/BookRepository.cs
+++ LooseLeaf.DataAccess/Repositories/BookRepository.cs
@@ -2,6 +2,7 @@
 using LooseLeaf.Business.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
+using LooseLeaf.DataAccess;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,15 @@ namespace LooseLeaf.DataAccess.Repositories
             _context = context;
         }
 
-        public async Task<IEnumerable<IBook>> GetAllBooks(IBookSearchParams searchParams) => throw new NotImplementedException();
+        public async Task<IEnumerable<IBook>> GetSpecificBooks(IBookSearchParams searchParams) 
+        {
+            var newParams = searchParams.ToString().ToLower();
+
+            var results = await _context.Books.Where(x => x.Title.ToLower().Contains(newParams) || 
+                x.Author.ToLower().Contains(newParams) || x.Genre.GenreName.ToLower().Contains(newParams)).ToListAsync();
+
+            return results.Select(x => ConvertToIBook(x));
+        }
 
         public async Task<IBook> GetBook(int bookId)
         {
@@ -29,5 +38,10 @@ namespace LooseLeaf.DataAccess.Repositories
         public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
 
         public async Task UpdateBook(int bookId) => throw new NotImplementedException();
+
+        private IBook ConvertToIBook(Book book)
+        {
+            return new Business.Models.Book(book.Title, book.Author, book.Isbn, book.GenreId);
+        }
     }
 }
--- LooseLeaf.Tests/IntegrationTests/BookRepositoryTests.cs
+++ LooseLeaf.Tests/IntegrationTests/BookRepositoryTests.cs
@@ -50,5 +50,93 @@ namespace LooseLeaf.Tests.IntegrationTests
             Assert.Equal(insertedBook.Isbn, book.Isbn);
             Assert.Equal(insertedBook.GenreId, book.GenreId);
         }
+
+        [Fact]
+        public async Task AddBook()
+        {
+            // arrange
+            const string title = "Gone Vith the Vend";
+            const string author = "Vladimir";
+            const long isbn = 9781234567890;
+
+            using var contextFactory = new TestLooseLeafContextFactory();
+            using (LooseLeafContext arrangeContext = contextFactory.CreateContext())
+            {
+                await contextFactory.CreateGenre(arrangeContext);
+                await arrangeContext.SaveChangesAsync();
+            }
+
+            Mock<IBook> fakeBook = new Mock<IBook>();
+            fakeBook.Setup(x => x.Title).Returns(title);
+            fakeBook.Setup(x => x.Author).Returns(author);
+            fakeBook.Setup(x => x.GenreId).Returns(1);
+            fakeBook.Setup(x => x.Isbn).Returns(isbn);
+
+            // act
+            using (LooseLeafContext actContext = contextFactory.CreateContext())
+            {
+                IBookRepository bookRepo = new BookRepository(actContext);
+
+                await bookRepo.AddBook(fakeBook.Object);
+                await actContext.SaveChangesAsync();
+            }
+
+            // assert
+            using (LooseLeafContext assertContext = contextFactory.CreateContext())
+            {
+                var book = await assertContext.Books.Include(x => x.Genre).SingleAsync();
+
+                Assert.Equal(title, book.Title);
+                Assert.Equal(author, book.Author);
+                Assert.Equal(isbn, book.Isbn);
+            }
+        }
+
+
+        [Fact]
+        public async Task GetSpecificBooks_ReturnList()
+        {
+            // arrange
+            const string title1 = "Brave Little Toaster";
+            const string author1 = "Lil Wayne";
+            long isbn1 = 1234567890123;
+            const string title2 = "Three Blind Mice";
+            const string author2 = "Celine Dion";
+            long isbn2 = 1234567890124;
+
+            using var contextFactory = new TestLooseLeafContextFactory();
+            using (LooseLeafContext arrangeContext = contextFactory.CreateContext())
+            {
+                await contextFactory.CreateGenre(arrangeContext);
+                await arrangeContext.SaveChangesAsync();
+
+                List<DataAccess.Book> books = new List<DataAccess.Book>()
+                    {
+                        new DataAccess.Book() { Title = title1, Author = author1, Isbn = isbn1, GenreId = 1 },
+                        new DataAccess.Book() { Title = title2, Author = author2, Isbn = isbn2, GenreId = 1 }
+                    };
+
+                await arrangeContext.AddRangeAsync(books);
+                await arrangeContext.SaveChangesAsync();
+
+            }
+
+            using (LooseLeafContext actContext = contextFactory.CreateContext())
+            {
+                await contextFactory.CreateGenre(actContext);
+                await actContext.SaveChangesAsync();
+
+                IBookRepository bookRepo = new BookRepository(actContext);
+                await bookRepo.GetSpecificBooks(new BookSearchParams());
+                await actContext.SaveChangesAsync();
+            };
+
+            // assert
+            using (LooseLeafContext assertContext = contextFactory.CreateContext())
+            {
+                var book = await assertContext.Books.Include(x => x.Genre).ToListAsync();
+                Assert.Equal(2, book.Count);
+            }
+        }
     }
 }
