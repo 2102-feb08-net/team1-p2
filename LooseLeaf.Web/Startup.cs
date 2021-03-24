60e99180ab324942342bdfd2826a36ba4d40329d
Bryson Ewell <brysonewell@outlook.com>
2021-03-24 11:19:07 -0400

    Fixed security hotspot

--- LooseLeaf.Web/Startup.cs
+++ LooseLeaf.Web/Startup.cs
@@ -83,18 +83,18 @@ namespace LooseLeaf.Web
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             //Turned off temporarily.
-            //if (env.IsDevelopment())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LooseLeaf.Web v1"));
             }
-            //else
-            //{
-            //    {
-            //        app.UseExceptionHandler("/home/error");
-            //    }
-            //}
+            else
+            {
+                {
+                    app.UseExceptionHandler("/home/error");
+                }
+            }
 
             app.UseHttpsRedirection();
 
